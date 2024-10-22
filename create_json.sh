635886fce4ae95f8e3a39426d9efd30a066eb1c9
Björn <pathmapper@posteo.de>
2024-10-22 07:15:34 +0200

    Do not update if JSON is empty

--- create_json.sh
+++ create_json.sh
@@ -7,7 +7,10 @@ unzip ./temp/wfsdata.zip -d ./temp/
 ogr2ogr -f GeoJSON -dialect SQLite -sql "SELECT art, name, schluessel, gmdschl FROM KatasterBezirk" ./temp/data.geojson ./temp/ALKIS-Vereinfacht/KatasterBezirk.shp
 jq -c --argfile katasteraemter katasteraemter.json '.features[] | select(.properties.art|contains("Gemarkungsteil/Flur")|not ) | {name: (.properties.name + " (" + (.properties.schluessel | tonumber | tostring | .[1:5]) +")"), schluessel: .properties.schluessel | tonumber | tostring | .[1:5], gmdschl: .properties.gmdschl | tonumber | tostring | .[0:4]} | .gmdschl |= $katasteraemter[.]' ./temp/data.geojson > ./temp/gemarkungen.txt
 jq -c '.features[] | select(.properties.art|contains("Gemarkungsteil/Flur")) | {name: .properties.name | tonumber | tostring, schluessel: .properties.schluessel | tonumber | tostring | .[1:5]}' ./temp/data.geojson > ./temp/fluren.txt
-jq -c -S --null-input --slurpfile gemarkungen ./temp/gemarkungen.txt --slurpfile fluren ./temp/fluren.txt 'reduce $gemarkungen[] as $i ({}; setpath([$i.gmdschl, $i.name];{schluessel: $i.schluessel, fluren: [$fluren[] | select(.schluessel == $i.schluessel).name]}))' > ./data/katasteraemter-gemarkungen-fluren-nrw.json
+jq -c -S --null-input --slurpfile gemarkungen ./temp/gemarkungen.txt --slurpfile fluren ./temp/fluren.txt 'reduce $gemarkungen[] as $i ({}; setpath([$i.gmdschl, $i.name];{schluessel: $i.schluessel, fluren: [$fluren[] | select(.schluessel == $i.schluessel).name]})) | if . == {} then empty else . end' > ./temp/katasteraemter-gemarkungen-fluren-nrw.json
+if [ -s ./temp/katasteraemter-gemarkungen-fluren-nrw.json ]; then 
+  mv ./temp/katasteraemter-gemarkungen-fluren-nrw.json ./data/katasteraemter-gemarkungen-fluren-nrw.json
+fi
 rm -r temp
 cd data
 md5sum katasteraemter-gemarkungen-fluren-nrw.json > katasteraemter-gemarkungen-fluren-nrw.json.md5
